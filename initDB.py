20bc215b47a04c0f8d850dd431064d202db8abfe
Rajiv-INFO2602 <60627113+Rajiv-INFO2602@users.noreply.github.com>
2021-04-23 19:17:05 -0400

    Update initDB.py

--- initDB.py
+++ initDB.py
@@ -4,18 +4,27 @@ import csv
 
 db.create_all(app=app)
 
-with open('ingrediants.csv', newline='') as csvfile:
+with open('products.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter= ',')
 
     for row in reader:
-        if row['item'] == '':
-            row['item'] = None
+        if row['no'] == '':
+            row['no'] = None
+        if row['products'] == '':
+            row['products'] = None
+        if row['brand'] == '':
+            row['brand'] = None
+        if row['size'] == '':
+            row['size'] = None
         if row['price'] == '':
             row['price'] = None
 
             ingrediants = Ingredients(
-                item= row['item'],
-                price= row['price']
+                no= row['no'],
+                products= row['products'],
+                products= row['brand'],
+                products= row['size'],
+                products= row['price']
             )
             db.session.add(item)
             db.session.commit()
