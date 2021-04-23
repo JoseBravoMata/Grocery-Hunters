e4bdbbcb55a85eb5192fac04aaa4d5afa3f139f7
Rajiv-INFO2602 <60627113+Rajiv-INFO2602@users.noreply.github.com>
2021-04-23 19:33:11 -0400

    Add files via upload

--- initDB.py
+++ initDB.py
@@ -1,22 +1,31 @@
 from main import app
-from models import db, Ingredients
+from models import db, Products
 import csv
 
 db.create_all(app=app)
 
-with open('ingrediants.csv', newline='') as csvfile:
+with open('products.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter= ',')
 
     for row in reader:
-        if row['item'] == '':
-            row['item'] = None
+        if row['no'] == '':
+            row['no'] = None
+        if row['products'] == '':
+            row['products'] = None
+        if row['brand'] == '':
+            row['brand'] = None
+        if row['size'] == '':
+            row['size'] = None
         if row['price'] == '':
             row['price'] = None
 
-            ingrediants = Ingredients(
-                item= row['item'],
+            products = Products(
+                no= row['no'],
+                products= row['products'],
+                brand= row['brand'],
+                size= row['size'],
                 price= row['price']
             )
-            db.session.add(item)
+            db.session.add(products)
             db.session.commit()
 print('database initialized!')
