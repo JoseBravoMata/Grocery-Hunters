4cdfe97baa972e2f38c5630641169993a97001f1
Rajiv-INFO2602 <60627113+Rajiv-INFO2602@users.noreply.github.com>
2021-04-24 17:00:08 -0400

    Update main.py

--- App/main.py
+++ App/main.py
@@ -44,28 +44,6 @@ app = create_app()
 app.app_context().push()
 db.create_all(app=app)
 
-with open('./App/products.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter= ',')
-
-    for row in reader:
-        if row['no'] == '':
-            row['no'] = None
-        if row['products'] == '':
-            row['products'] = None
-        if row['brand'] == '':
-            row['brand'] = None
-        if row['price'] == '':
-            row['price'] = None
-
-            products = Products(
-                no= row['no'],
-                products= row['products'],
-                brand= row['brand'],
-                price= row['price']
-            )
-            db.session.add(products)
-            db.session.commit()
-
 app.register_blueprint(api_views)
 app.register_blueprint(user_views)
 
